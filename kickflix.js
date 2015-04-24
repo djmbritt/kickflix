6e3dc9a586be4f48a3de80cfde442008deca62d6
davbritt <djmbritt@gmail.com>
2015-04-24 15:17:50 +0200

    beautified

--- kickflix.js
+++ kickflix.js
@@ -5,33 +5,33 @@ var readline = require('readline');
 var proc = require('child_process');
 var torrents;
 
-function getLogTor(query, callback){
-	kickass(query, function(err, response) {
-		if (err) console.error(err);
-		torrents = response.list;
-		console.log(response.description + '\n' + 'total_results: ' + response.total_results);
-		
-		var len = response.list.length;
-		for(var i = 0; i < len; i++){
-			console.log(i + '. \t' + response.list[i].title + '\n' + '\t' + response.list[i].pubDate + '\n' + '\t' + 'Peers: ' + response.list[i].peers + '\t' + 'Seeds: ' + response.list[i].seeds + '\t' + 'Votes: ' + response.list[i].votes);
-		}
-		callback();
-	});
+function getLogTor(query, callback) {
+    kickass(query, function(err, response) {
+        if (err) console.error(err);
+        torrents = response.list;
+        console.log(response.description + '\n' + 'total_results: ' + response.total_results);
+
+        var len = response.list.length;
+        for (var i = 0; i < len; i++) {
+            console.log(i + '. \t' + response.list[i].title + '\n' + '\t' + response.list[i].pubDate + '\n' + '\t' + 'Peers: ' + response.list[i].peers + '\t' + 'Seeds: ' + response.list[i].seeds + '\t' + 'Votes: ' + response.list[i].votes);
+        }
+        callback();
+    });
 }
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-var kickflix = rl.question('Search KickAss: ', function(answer){
-	getLogTor(answer, function() {
-		rl.question('Choose torrent to stream (type in the number): ', function(n){
-			proc.exec(process.cwd() + '/node_modules/peerflix/app.js -v -r ' + torrents[n].torrentLink, function(err, ouput, stdin) {
-				if(err) console.error(err);
-				output.pipe(stdout);
-			});
-		});
+var kickflix = rl.question('Search KickAss: ', function(answer) {
+    getLogTor(answer, function() {
+        rl.question('Choose torrent to stream (type in the number): ', function(n) {
+            proc.exec(process.cwd() + '/node_modules/peerflix/app.js -v -r ' + torrents[n].torrentLink, function(err, ouput, stdin) {
+                if (err) console.error(err);
+                output.pipe(stdout);
+            });
+        });
 
-	});
+    });
 });
--- test/writeJson.js
+++ test/writeJson.js
@@ -6,14 +6,14 @@ var search = process.argv[2].toString();
 var fileName = search + '.json';
 
 function getTor(query) {
-	kick(query, function(err, data) {
-		if (err) console.error(err);
-		blob = JSON.stringify(data);
-		fs.writeFile(fileName, blob, 'utf8', function(err) {
-			if(err) console.error(err);
-			console.log('Check out file: ' + 'kick.json');
-		});
-	});	
+    kick(query, function(err, data) {
+        if (err) console.error(err);
+        blob = JSON.stringify(data);
+        fs.writeFile(fileName, blob, 'utf8', function(err) {
+            if (err) console.error(err);
+            console.log('Check out file: ' + 'kick.json');
+        });
+    });
 }
 
 getTor(search);
