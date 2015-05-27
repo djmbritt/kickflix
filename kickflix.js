61999cf7cb1d365de3338483f59828cbd6e8bc2f
davbritt <djmbritt@gmail.com>
2015-05-27 18:35:19 +0200

    Changed Base URL, added checkuptime()

--- kickflix.js
+++ kickflix.js
@@ -1,35 +1,49 @@
 #!/usr/bin/env node
 
-var _ = require('lodash');
+var http = require('http');
 var kickass = require('kickass-torrent');
 var readline = require('readline');
 var spawn = require('child_process').spawn;
+
+var BASEURL = 'http://kat.cr' || 'http://kickassto.co/' || 'http://katproxy.is/' || 'http://thekat.tv/' ;
 var pageNumber = 0;
 var torrents;
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-function getLogTor(query, callback) {
+function checkUpTime(url, cb) {
+    http.get(url, function(response) {
+        console.log('Kickass is up and running @', response.client._host);
+        console.log('HTTP Response Status:', response.statusCode);
+        cb();
+    });
+}
+
+function getLogTor(query, cb) {
     kickass(query, function(err, response) {
         if (err) console.error(err);
 
-        var len = 10;
         torrents = response.list;
+
+        //check length of the returened array and assign appropriate value;
+        var len;
+        if(torrents.length < 10) len = torrents.length;
+        len = 10;
 
         rl.write(response.description + '\n' + 'total_results: ' + response.total_results + '\n');
 
         for (var i = 0; i < len; i++) {
-            rl.write(   i + '. \t' + 
-                        torrents[i].title + '\n' + '\t' +  
-                        torrents[i].pubDate + '\n' + '\t' + 
-                        'Peers: ' + torrents[i].peers + '\t' + 
-                        'Seeds: ' + torrents[i].seeds + '\t' + 
-                        'Votes: ' + torrents[i].votes + '\t' + 
+            rl.write(   i + '. \t' +
+                        torrents[i].title + '\n' + '\t' +
+                        torrents[i].pubDate + '\n' + '\t' +
+                        'Peers: ' + torrents[i].peers + '\t' +
+                        'Seeds: ' + torrents[i].seeds + '\t' +
+                        'Votes: ' + torrents[i].votes + '\t' +
                         'Size: '  + Math.round(Math.pow(10, -6)*torrents[i].size) + ' Mb' + '\n'
             );
         }
 
-        callback();
+        cb();
     });
 }
 
@@ -44,14 +58,18 @@ function ask() {
                     field: 'seeders', //seeders, leechers, time_add, files_count, empty for best match
                     order: 'desc', //asc or desc
                     page: pageNumber, //page count, obviously
-                    url: 'http://kickass.to', //changes site default url (http://kickass.to)
-            }, function(){
+                    url: BASEURL //changes site default url (http://kickass.to)
+                }, function input(){
                 rl.question('Press enter to search again or input number to stream torrent: ', function (n) {
-                    if(n.length === 0) {
-                        ask();
-                    }else if(_.isNumber(Number(n))){
+
+                    if(n.length === 0) return ask();
+
+                    if(typeof Number(n) === 'number'){
+
+                        //spawn child process, and start peerflix with flags -v -r.
                         var vlc = spawn('./app.js', ['-v', '-r', torrents[n].torrentLink], {cwd: __dirname + '/node_modules/peerflix/'});
-                        
+
+                        //pipe output to display
                         vlc.stdout.pipe(process.stdout);
 
                         vlc.stderr.on('data', function (data) {
@@ -60,16 +78,16 @@ function ask() {
 
                         vlc.on('close', function (code) {
                           rl.write('child process exited with code ' + code);
-                          process.exit()
+                          process.exit();
                         });
-                    
+
                     } else {
                         ask();
-                    };
+                    }
                 });
             });
         }
     });
 }
 
-ask();
+checkUpTime(BASEURL, ask);
