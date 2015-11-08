323b7a5875295f80533865d74518b1af4dfc14c3
davbritt <djmbritt@gmail.com>
2015-11-08 22:49:18 +0100

    Refactored

---
+++ kick.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+var rl = require('readline');
+var kickass = require('kat-api');
+var spawn = require('child_process').spawn;
+var chalk = require('chalk');
+var checkUpTime = require('./lib/upcheck');
+// var commander = require('commander');
+
+// keypress(process.stdin);
+
+var readline = rl.createInterface(process.stdin, process.stdout);
+var torrents;
+var pageNumber = 1;
+
+
+
+function kickAssQuery(kickQuery, cb) {
+  kickass.search({
+      query: kickQuery,
+      page: pageNumber,
+      verified: 1,
+      sort_by: 'seeders'
+    }, kickQuery)
+    .then(function(data) {
+      torrents = data.results;
+      console.log(chalk.bgYellow.black("PageNumber: " + data.page + " TotalPages: " + data.total_pages + " TotalResults: " + data.total_results));
+      for (var i = 0; i < torrents.length; i++) {
+        readline.write(i + '. \t' +
+          chalk.magenta.bold(torrents[i].title) + '\n' + '\t' +
+          chalk.blue(torrents[i].pubDate.slice(0, -5)) + '\n' + '\t' +
+          'Seeders:' + chalk.yellow(torrents[i].seeds) + ' - ' +
+          'Leechers:' + chalk.yellow(torrents[i].leechs) + ' - ' +
+          'Peers:' + chalk.yellow(torrents[i].peers) + ' - ' +
+          'Votes:' + chalk.yellow(torrents[i].votes) + ' - ' +
+          'Size:' + chalk.yellow(Math.round(Math.pow(10, -6) * torrents[i].size)) + 'Mb' + '\n'
+        );
+      }
+      cb();
+    })
+    .catch(err => console.error(err));
+}
+
+function reQuery(answer) {
+
+  kickAssQuery(answer, function() {
+
+    var askForInput = "[enter] to search again, [m] load next page, [n] previous page, [number] to stream torrent: ";
+    readline.question(askForInput, function(n) {
+      if (n.length === 0) return ask();
+
+      if (n == 'm') {
+        pageNumber++;
+        return reQuery(answer);
+      }
+
+      if(n == 'n'){
+      	if(pageNumber>1) pageNumber--;
+      	return reQuery(answer);
+      }
+
+      if (typeof Number(n) === 'number') {
+        var vlc = spawn('./app.js', ['-v', '-r', torrents[n].magnet], {
+          cwd: __dirname + '/node_modules/peerflix'
+        });
+
+        vlc.stderr.on('data', function(data) {
+          console.error('\n Error: ' + data + '\n');
+        });
+
+        vlc.on('close', function(data) {
+          readline.write('\n Something went wrong with the child_process, Error: ' + data + '\n');
+        });
+      } else {
+        return ask();
+      }
+    });
+
+  });
+};
+
+function ask(answer) {
+  readline.question('Search Kickass: ', function(answer) {
+    if (answer.length === 0) {
+      console.log('Input your query...');
+      return ask();
+    } else {
+      reQuery(answer);
+    }
+  });
+}
+
+ask();
--- kickflix.js
+++ kickflix.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 
 var readline = require('readline');
-var kickass = require('kickass-torrent');
+var kickass = require('kat-api');
 var spawn = require('child_process').spawn;
 var colors = require('colors');
 var checkUpTime = require('./lib/upcheck');
 
 
-var BASEURL = 'http://kat.cr' || 'http://kickassto.co/' || 'http://katproxy.is/' || 'http://thekat.tv/' ;
 var pageNumber = 0;
 var torrents;
 
@@ -15,80 +14,78 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 
 
 function getLogTor(readline, query, cb) {
-    kickass(query, function(err, response) {
-        if (err) console.error(err);
+  kickass(query, function(err, response) {
+    if (err) console.error(err);
 
-        torrents = response.list;
+    torrents = response.list;
 
-        //check length of the returened array and assign appropriate value;
-        var len;
-        torrents.length < 10 ? len = torrents.length : len = 10;
+    //check length of the returened array and assign appropriate value;
+    var len;
+    torrents.length < 10 ? len = torrents.length : len = 10;
 
-        //readline.createInterface
-        readline.write(response.description + '\n' + 'total_results: ' + response.total_results + '\n');
+    //readline.createInterface
+    readline.write(response.description + '\n' + 'total_results: ' + response.total_results + '\n');
 
-        for (var i = 0; i < len; i++) {
-            readline.write( i + '. \t' +
-                            colors.magenta.bold(torrents[i].title) + '\n' + '\t' +
-                            colors.blue(torrents[i].pubDate.slice(0, -5)) + '\n' + '\t' +
-                            'Peers: ' + colors.yellow(torrents[i].peers) + '\t' +
-                            'Seeds: ' + colors.yellow(torrents[i].seeds) + '\t' +
-                            'Votes: ' + colors.yellow(torrents[i].votes) + '\t' +
-                            'Size: '  + colors.yellow(Math.round(Math.pow(10, -6)*torrents[i].size)) + ' Mb' + '\n'
-            );
-        }
-
-        cb();
-    });
+    for (var i = 0; i < len; i++) {
+      readline.write(i + '. \t' +
+        colors.magenta.bold(torrents[i].title) + '\n' + '\t' +
+        colors.blue(torrents[i].pubDate.slice(0, -5)) + '\n' + '\t' +
+        'Peers: ' + colors.yellow(torrents[i].peers) + '\t' +
+        'Seeds: ' + colors.yellow(torrents[i].seeds) + '\t' +
+        'Votes: ' + colors.yellow(torrents[i].votes) + '\t' +
+        'Size: ' + colors.yellow(Math.round(Math.pow(10, -6) * torrents[i].size)) + ' Mb' + '\n'
+      );
+    }
+    cb();
+  });
 }
 
 
 
 
 function ask(answer) {
-    rl.question('Search Kickass: ', function(answer) {
-        if(answer.length === 0) {
-            console.log("type in something");
-            return ask();
-        }else{
-            getLogTor(rl, 
-                {   q: answer, //actual search term
-                    field: 'seeders', //seeders, leechers, time_add, files_count, empty for best match
-                    order: 'desc', //asc or desc
-                    page: pageNumber, //page count, obviously
-                    url: BASEURL //changes site default url (http://kickass.to)
-                }, 
-                function input(){
-                    rl.question('Press enter to search again or input number to stream torrent: ', function (n) {
+  rl.question('Search Kickass: ', function(answer) {
+    if (answer.length === 0) {
+      console.log("type in something");
+      return ask();
+    } else {
+      getLogTor(rl, {
+          q: answer, //actual search term
+          field: 'seeders', //seeders, leechers, time_add, files_count, empty for best match
+          order: 'desc', //asc or desc
+          page: pageNumber, //page count, obviously
+          url: BASEURL //changes site default url (http://kickass.to)
+        },
+        function input() {
+          rl.question('Press enter to search again or input number to stream torrent: ', function(n) {
 
-                        if(n.length === 0) return ask();
-                        if(typeof Number(n) === 'number'){
+            if (n.length === 0) return ask();
+            if (typeof Number(n) === 'number') {
 
-                            //spawn child process, and start peerflix with flags -v -r.
-                            var vlc = spawn('./app.js', 
-                                            ['-v', '-r', torrents[n].torrentLink], 
-                                            {cwd: __dirname + '/node_modules/peerflix/'}
-                            );
+              //spawn child process, and start peerflix with flags -v -r.
+              var vlc = spawn('./app.js', ['-v', '-r', torrents[n].torrentLink], {
+                cwd: __dirname + '/node_modules/peerflix/'
+              });
 
-                            //pipe output to display, need to figure out how to not make it scroll
-                            vlc.stdout.pipe(process.stdout);
+              //pipe output to display, need to figure out how to not make it scroll
+              vlc.stdout.pipe(process.stdout);
 
-                            vlc.stderr.on('data', function (data) {
-                              console.error('stderr: ' + data);
-                            });
+              vlc.stderr.on('data', function(data) {
+                console.error('stderr: ' + data);
+              });
 
-                            vlc.on('close', function (code) {
-                              rl.write('child process exited with code ' + code);
-                              process.exit();
-                            });
+              vlc.on('close', function(code) {
+                rl.write('child process exited with code ' + code);
+                process.exit();
+              });
 
-                        } else {
-                            return ask();
-                        }
-                });
-            });
-        }
-    });
+            } else {
+              return ask();
+            }
+          });
+        });
+    }
+  });
 }
 
 checkUpTime(BASEURL, ask);
